daff6c18e1df1e4dc10f13ba3543e6bde4ad8c2b
haven <haven@hellotalk.cn>
2024-02-29 18:18:23 +0800

    save

--- algo/sort_test.go
+++ algo/sort_test.go
@@ -32,9 +32,7 @@ func bubbleSort(nums []int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := 0; j < len(nums)-1-i; j++ {
 			if nums[j] > nums[j+1] {
-				temp := nums[j]
-				nums[j] = nums[j+1]
-				nums[j+1] = temp
+				nums[j], nums[j+1] = nums[j+1], nums[j]
 			}
 		}
 	}
