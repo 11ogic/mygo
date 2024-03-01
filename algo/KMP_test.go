f33d76b3e92bfea1dd54e461d2b85222335653b8
haven <haven@hellotalk.cn>
2024-03-02 00:24:31 +0800

    save

--- algo/KMP_test.go
+++ algo/KMP_test.go
@@ -2,6 +2,28 @@ package algo
 
 import "testing"
 
-func TestKMP(t *testing.T) {
+func BF(str string, target string) int {
+	strRune := []rune(str)
+	targetRune := []rune(target)
+	for i := 0; i <= len(strRune)-len(targetRune); i++ {
+		match := true
+		for j, v := range targetRune {
+			if strRune[i+j] != v {
+				match = false
+				break
+			}
+		}
+		if match {
+			return i
+		}
+	}
+	return -1
+}
 
+func KMP(str string, target string) int {
+	return -1
+}
+
+func TestKMP(t *testing.T) {
+	TimeSpent(BF, "今天天气真不错", "真不错")
 }
