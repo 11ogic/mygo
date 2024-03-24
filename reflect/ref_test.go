b41de9c2d3a2aacb4b933ad0336ea36f183224e3
haven <haven@hellotalk.cn>
2024-03-24 15:00:01 +0800

    save

--- reflect/ref_test.go
+++ reflect/ref_test.go
@@ -6,18 +6,26 @@ import (
 	"testing"
 )
 
-type myInt int8
+type Cal struct {
+	Num1 int
+	Num2 int
+}
 
-func test(i interface{}) {
-	rVal := reflect.ValueOf(i)
-	rTyp := reflect.TypeOf(i)
-	//num := rVal.Int()
-	//typ := rTyp.Kind()
-	fmt.Printf("type: %T, value: %v \n", rVal, rVal)
-	fmt.Printf("type: %T, value: %v \n", rTyp, rTyp)
+func (c *Cal) GetSub(name string) {
+	fmt.Printf("%v 完成了减法运算，%v - %v = %v", name, c.Num1, c.Num2, c.Num1-c.Num2)
 }
 
 func TestRef(t *testing.T) {
-	var num myInt = 1
-	test(num)
+	cal := Cal{}
+	cVal := reflect.ValueOf(&cal)
+	cTyp := reflect.TypeOf(&cal)
+	for i := 0; i < cVal.Elem().NumField(); i++ {
+		fmt.Printf("name: %v, value: %v \n", cTyp.Elem().Field(i).Name, cVal.Elem().Field(i))
+	}
+	cVal.Elem().FieldByName("Num1").SetInt(8)
+	cVal.Elem().FieldByName("Num2").SetInt(5)
+
+	cVal.MethodByName("GetSub").Call([]reflect.Value{
+		reflect.ValueOf("Tom"),
+	})
 }
