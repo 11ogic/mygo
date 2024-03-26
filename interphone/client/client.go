048c21bbbac3b6ef9035019ee998e13c81d934d3
haven <haven@hellotalk.cn>
2024-03-27 00:44:13 +0800

    save

--- interphone/client/client.go
+++ interphone/client/client.go
@@ -19,12 +19,17 @@ func main() {
 	message := make([]byte, 1024)
 	for {
 		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		conn.Write([]byte(line))
+
 		n, err := conn.Read(message)
 		if err != nil {
 			fmt.Println(err)
 			break
 		}
-		fmt.Println(message[:n])
-		conn.Write([]byte(line))
+		fmt.Println(string(message[:n]))
 	}
 }
