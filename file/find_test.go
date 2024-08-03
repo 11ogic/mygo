c7236dfd30e26528ea49d1c0a61129449e503ccf
haven <haven@hellotalk.cn>
2024-08-03 23:52:21 +0800

    test

--- file/find_test.go
+++ file/find_test.go
@@ -3,85 +3,32 @@ package file
 import (
 	"fmt"
 	"os"
-	"sync"
 	"testing"
 	"time"
 )
 
 var (
-	mu             sync.Mutex
-	matched        int
-	query          = "package.json"
-	workerCount    = 0
-	maxWorkerCount = 32
-	searchRequest  = make(chan string)
-	workDone       = make(chan bool)
-	foundFile      = make(chan bool)
+	matches = 0
+	query   = "package.json"
 )
 
 func TestFind(t *testing.T) {
 	start := time.Now()
-	increaseWorkerCount()
-	go search("/Users/", true)
-	waitForWorker()
-	fmt.Println("matched: ", matched)
-	fmt.Println(time.Since(start))
+	searchFile("/Users/haven/code/keep_and_increase")
+	fmt.Printf("matches: %d \n", matches)
+	fmt.Println(start)
 }
 
-func increaseWorkerCount() {
-	mu.Lock()
-	defer mu.Unlock()
-	workerCount++
-}
-
-func decrementWorkerCount() {
-	mu.Lock()
-	defer mu.Unlock()
-	workerCount--
-}
-
-func getWorkerCount() int {
-	mu.Lock()
-	defer mu.Unlock()
-	return workerCount
-}
-
-func waitForWorker() {
-	for {
-		select {
-		case path := <-searchRequest:
-			increaseWorkerCount()
-			go search(path, true)
-		case <-workDone:
-			decrementWorkerCount()
-			if getWorkerCount() == 0 {
-				return
-			}
-		case <-foundFile:
-			matched++
-		}
-	}
-}
-
-func search(path string, master bool) {
-	defer func() {
-		if master {
-			workDone <- true
-		}
-	}()
+func searchFile(path string) {
 	files, err := os.ReadDir(path)
 	if err == nil {
 		for _, file := range files {
 			name := file.Name()
 			if name == query {
-				foundFile <- true
+				matches++
 			}
 			if file.IsDir() {
-				if getWorkerCount() < maxWorkerCount {
-					searchRequest <- path + name + "/"
-				} else {
-					search(path+name+"/", false)
-				}
+				searchFile(path + "/" + name)
 			}
 		}
 	}
