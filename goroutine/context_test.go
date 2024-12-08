e58c1273294371885f072acf5ac57c44e7946a59
haven <haven@hellotalk.cn>
2024-12-09 00:32:43 +0800

    prac

--- goroutine/context_test.go
+++ goroutine/context_test.go
@@ -1,48 +1,46 @@
 package goroutine
 
 import (
-    "fmt"
-    "sync"
-    "testing"
-    "time"
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
 )
 
 var (
-    wg   = sync.WaitGroup{}
-    exit = make(chan bool)
+	wg = sync.WaitGroup{}
 )
 
 func TestContext(t *testing.T) {
-    //ctx, cancel := context.WithCancel(context.Background())
-    for i := 0; i < 3; i++ {
-        wg.Add(1)
-        go func() {
-            ip, err := getIP(exit)
-            fmt.Println(ip, err, "@@@")
-        }()
-    }
-    go func() {
-        time.Sleep(2 * time.Second)
-        exit <- true
-        //cancel()
-    }()
-    wg.Wait()
-    fmt.Println("Done...")
+	ctx, cancel := context.WithCancel(context.Background())
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func() {
+			ip, err := getIP(ctx)
+			fmt.Println(ip, err, "@@@")
+		}()
+	}
+	go func() {
+		time.Sleep(2 * time.Second)
+		cancel()
+	}()
+	wg.Wait()
+	fmt.Println("Done...")
 }
 
-func getIP(exit chan bool) (ip string, err error) {
-    go func() {
-        select {
-        case <-exit:
-            wg.Done()
-            fmt.Println("Exit...")
-            //err = ctx.Err()
-            return
-        }
-    }()
-    time.Sleep(4 * time.Second)
-    ip = "0.0.0.0"
-    fmt.Println("Get IP...")
-    wg.Done()
-    return ip, nil
+func getIP(ctx context.Context) (ip string, err error) {
+	go func() {
+		select {
+		case <-ctx.Done():
+			wg.Done()
+			err = ctx.Err()
+			return
+		}
+	}()
+	time.Sleep(4 * time.Second)
+	ip = "0.0.0.0"
+	fmt.Println("Get IP...")
+	wg.Done()
+	return ip, nil
 }
